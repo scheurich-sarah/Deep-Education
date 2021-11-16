cec8a525c279c2815b4d59689e6a7d5feb5455e4
Sarah C  Scheurich <sarah.scheurich@richmond.edu>
2021-11-15 22:27:52 -0500

    Simple version where you can test that multithread version is faster by modifying the invoke_gspmm to call _gspmm_mt. Trying to make something better. Also figured out whatbroke repo last time. Copying pybind11 from CS636 repo, there was a pybind11/.git file that confused the repo. Deleting it fixed the issue.

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <chrono>
 #include <iostream>
 #include <limits>
 
@@ -6,6 +7,7 @@
 
 using std::cout;
 using std::endl;
+using namespace std::chrono;
 
 int THD_COUNT = 1;
 
@@ -15,6 +17,7 @@ using std::string;
 void _gspmm(csr_t* snaph, array2d_t<float> & input, array2d_t<float> & output, 
                      op_t op, bool reverse, bool norm /*= true*/)
 {
+    auto start = high_resolution_clock::now();
     //cout << "spmm " << op << ", reverse = " << reverse << endl;
 
     // input row count always = # nodes
@@ -147,6 +150,9 @@ void _gspmm(csr_t* snaph, array2d_t<float> & input, array2d_t<float> & output,
 	    }
 	    */
     }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop-start);
+    cout << "single thread gspmm pass takes " << duration.count() << endl;
 
     //The core logic goes here.    
     // needs to be independent of any tensor data struc
@@ -161,6 +167,76 @@ void _gspmm(csr_t* snaph, array2d_t<float> & input, array2d_t<float> & output,
     // If in backward, normalize it first, else normalize it after computation
 }
 
+// multi threaded version
+void _gspmm_mt(csr_t* snaph, array2d_t<float> & input, 
+		array2d_t<float> & output, op_t op,
+		bool reverse, bool norm /*= true*/) {
+
+    auto start_mt = high_resolution_clock::now();
+    // get the offset and nebrs arrays
+    vid_t* offset = snaph->get_offset_ptr();
+    vid_t* nebrs = snaph->get_nebrs_ptr();
+    
+    #pragma omp parallel for
+    for (int64_t i = 0; i<input.row_count; i++){
+
+	    // get node degree
+	    vid_t node_degree = snaph->get_degree(i);
+	    // cout<< "node "<<i<<" degree = "<< node_degree<< endl;
+
+	    // normalization procedure
+	    // normalize arrays before summing if in backward pass
+	    // normalize after summing all arrays if in forward pass
+	    float pre_sum_norm; float post_sum_norm;
+	    if (reverse){ // backward pass
+		    post_sum_norm = (float) 1;
+		    pre_sum_norm = (float) node_degree;
+	    } else { // forward pass
+		    pre_sum_norm = (float) 1;
+		    post_sum_norm = (float) node_degree;
+	    }
+
+	    // pass pointer to input array row to the output array
+	    // the GCN paper specifies that it adds self connections
+	    // to the adjacency matrix, so we need to include the node
+	    // in the sum
+	    // memcpy(output[i], input[i], input.col_count*sizeof(input[i]));
+	    for (int k = 0 ; k<input.col_count; k++){
+		    output[i][k] = input[i][k]/pre_sum_norm;
+	    }
+
+	    // get node neighbors
+	    vid_t start_idx = offset[i];
+	    vid_t end_idx = offset[i + 1];
+	
+	    // loop through neighbors
+	    // perform column wise add for all nebr features
+	    // can loop through all neighbors in parallel
+
+	    //#pragma omp parallel for
+	    for (int64_t j= start_idx; j < end_idx; j++) {
+		vid_t neb = nebrs[j];
+	
+
+		// loop through columns and add to output
+		for (int64_t k = 0; k < input.col_count; k++){
+			output[i][k] += input[neb][k]/pre_sum_norm;
+		}
+	    }
+
+	    // do post-sum norm (will not change if in backward pass)
+	    for (int k = 0 ; k<input.col_count; k++){
+		    output[i][k] = output[i][k]/post_sum_norm;
+	    }
+
+    }
+    auto stop_mt = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop_mt-start_mt);
+    cout << "multi thread gspmm pass takes " << duration.count() << endl;
+
+}
+
+
 // The signature for this function is in kernel.h
 // very simple func,just pass a couple things
 // one of which needs to be a pointer
@@ -173,12 +249,32 @@ void invoke_gspmm(graph_t& graph, array2d_t<float> & input_array, array2d_t<floa
 	 // backward computation uses csr
 	 // normalzing involves dividing each input param/tensor
 	 // by it's degree, which you need to calc
-         return _gspmm(&graph.csr, input_array, output_array, eSUM, reverse, norm);
+         return _gspmm_mt(&graph.csr, input_array, output_array, eSUM, reverse, norm);
     } else {
 	 //cout<<"doing forward computation"<< endl;
 	 // forward computation uses csc, the transpose of csr
 	 // do GAS then normalize
-         return _gspmm(&graph.csc, input_array, output_array, eSUM, reverse, norm);
+         return _gspmm_mt(&graph.csc, input_array, output_array, eSUM, reverse, norm);
+    }
+
+}
+
+void invoke_gspmm_mt(graph_t& graph, array2d_t<float> & input_array, array2d_t<float> & output_array,
+                 bool reverse, bool norm /*= true*/)
+{
+    if (reverse) {
+	 //cout<<"doing backpropagation"<< endl;
+	 // backward computation uses csr
+	 // normalzing involves dividing each input param/tensor
+	 // by it's degree, which you need to calc
+         return _gspmm_mt(&graph.csr, input_array,
+			 output_array, eSUM, reverse, norm);
+    } else {
+	 //cout<<"doing forward computation"<< endl;
+	 // forward computation uses csc, the transpose of csr
+	 // do GAS then normalize
+         return _gspmm_mt(&graph.csc, input_array,
+			 output_array, eSUM, reverse, norm);
     }
 
 }
